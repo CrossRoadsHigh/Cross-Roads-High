f1d8f7a16265a835ee6cd1c0cbf4fa5da6a9b33a
JamesSteendyk <james.steendyk@connect.qut.edu.au>
2021-10-18 21:36:39 +1000

    Re-imported everything and added enemies, pickups and teleport spots

---
+++ Assignment 3 Mechanics Base/Assets/Scripts/ToDownstairs.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToDownstairs : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject teleportPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!player)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            collision.transform.position = teleportPos.transform.position;
+        }
+    }
+}
