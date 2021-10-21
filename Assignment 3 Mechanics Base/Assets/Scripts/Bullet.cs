12c528183c709a78bfb0c454cb59a1d7c7af28fa
Josh <josh.tulleners@gmail.com>
2021-10-21 17:27:04 +1000

    Gunshots now hit the walls
    and other minor hitbox changes

--- Assignment 3 Mechanics Base/Assets/Scripts/Bullet.cs
+++ Assignment 3 Mechanics Base/Assets/Scripts/Bullet.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class Bullet : Projectile {
 
     public override void OnTriggerEnter(Collider otherObject) {
-
-        if (otherObject.tag == "Enemy") { 
+        if (otherObject.tag == "Enemy") {
             otherObject.GetComponent<Enemy>().takeDamage(damage);
             Instantiate(hitEffect, transform.position, transform.rotation);
             Destroy(this.gameObject);
         }
-        else if (otherObject.tag == "Environment") {
+        else if (otherObject.gameObject.tag == "Environment")
+        {
             Instantiate(hitEffect, transform.position, transform.rotation);
             Destroy(this.gameObject);
         }
