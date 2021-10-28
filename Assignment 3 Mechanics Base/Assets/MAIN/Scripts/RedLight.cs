0b7f0a908a46174753a1d5649c21dd3804504c25
Josh <josh.tulleners@gmail.com>
2021-10-28 21:19:43 +1000

    Small lighting changes

---
+++ Assignment 3 Mechanics Base/Assets/MAIN/Scripts/RedLight.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedLight : MonoBehaviour
+{
+    public float speed = 1;
+
+    public float y = 0;
+
+    void Update()
+    {
+        y += speed * Time.deltaTime;
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, y, transform.rotation.eulerAngles.z);
+    }
+}
