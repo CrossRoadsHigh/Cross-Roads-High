57f80ef1500c14638628453772683f1a7cdccd35
Joshua Kennedy <61539773+PureIrishLad331@users.noreply.github.com>
2021-10-31 13:18:41 +1000

    Added ending audio + Fixed flame enemy sound
    - Added audio that wraps up the story at the end of level 3
    - Made flame enemy fire audio a proximity sound so that it was less obnoxious
    - Made it so player shooting and flamethrower audio stops upon death

--- Assignment 3 Mechanics Base/Assets/Scripts/PlayNarrativeClip.cs
+++ Assignment 3 Mechanics Base/Assets/Scripts/PlayNarrativeClip.cs
@@ -7,12 +7,16 @@ public class PlayNarrativeClip : MonoBehaviour
 {
     public AudioSource thisSource;
     public AudioSource musicSource;
+    public PlayNarrativeClip nextNarrativeClip;
     public List<AudioSource> audioSources;
     public bool startedPlaying;
     public string speakerName;
 
     public GameObject narrativeGUI;
     public Text speakerNameText;
+
+    public bool hasTrigger;
+    public GameObject triggerObject;
 
     public void Start()
     {
@@ -25,7 +29,16 @@ public class PlayNarrativeClip : MonoBehaviour
         {
             musicSource.volume = 0.8f;
             narrativeGUI.SetActive(false);
+
+            if (nextNarrativeClip != null)
+                nextNarrativeClip.Play();          
+
             Destroy(this.gameObject);
+        }
+
+        if (hasTrigger && !triggerObject.active && !startedPlaying)
+        {
+            Play();
         }
     }
 
@@ -33,19 +46,24 @@ public class PlayNarrativeClip : MonoBehaviour
     {
         if (other.tag == "Player" && !thisSource.isPlaying)
         {
-            startedPlaying = true;
-            thisSource.Play();
-            musicSource.volume = 0.4f;
-            speakerNameText.text = speakerName;
-            narrativeGUI.SetActive(true);
+            Play();
+        }
+    }
 
-            for (int i = 0; i < audioSources.Count; i++)
+    public void Play()
+    {
+        startedPlaying = true;
+        thisSource.Play();
+        musicSource.volume = 0.4f;
+        speakerNameText.text = speakerName;
+        narrativeGUI.SetActive(true);
+
+        for (int i = 0; i < audioSources.Count; i++)
+        {
+            if (audioSources[i] != null && audioSources[i].isPlaying)
             {
-                if (audioSources[i] != null && audioSources[i].isPlaying)
-                {
-                    audioSources[i].Stop();
-                    Destroy(audioSources[i].gameObject);
-                }
+                audioSources[i].Stop();
+                Destroy(audioSources[i].gameObject);
             }
         }
     }
--- Assignment 3 Mechanics Base/Assets/Scripts/PlayerAvatar.cs
+++ Assignment 3 Mechanics Base/Assets/Scripts/PlayerAvatar.cs
@@ -55,6 +55,10 @@ public class PlayerAvatar : MonoBehaviour {
             Movement();
             Shooting();
         }
+        else
+        {
+            thisAudioSource.Stop();
+        }
 
         if (Input.GetKeyDown(KeyCode.Escape))
         {
